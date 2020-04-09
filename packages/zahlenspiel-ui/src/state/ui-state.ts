e3f96e3794925f0bfe2606d8acd8d14b238de58f
Simon Hofmann <mail@simon-hofmann.org>
2020-04-10 00:04:27 +0200

    Dedicated UI state

---
+++ packages/zahlenspiel-ui/src/state/ui-state.ts
@@ -0,0 +1,20 @@
+import {Card, CardStack, GameStates, Player} from "zahlenspiel-shared-entities";
+
+export interface UIState {
+    self?: Player;
+    currentState: GameStates;
+    couldFinish: boolean;
+    players: Player[];
+    currentPlayer?: Player;
+    cardStacks?: CardStack[];
+    selectedCardStack?: CardStack;
+    cardDeck: Card[];
+    totalRemainingCards?: number;
+    remainingCardsInDeck?: number;
+}
+export const initialState: UIState = {
+    currentState: GameStates.WAITING,
+    couldFinish: false,
+    players: [],
+    cardDeck: [],
+}
