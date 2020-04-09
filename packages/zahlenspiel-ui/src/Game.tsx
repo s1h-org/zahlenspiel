da2e9e762f50e6e22f4bd13fdf42a9d143e7a0df
Simon Hofmann <mail@simon-hofmann.org>
2020-04-10 00:06:46 +0200

    Switched to using reducer for state handling

--- packages/zahlenspiel-ui/src/Game.tsx
+++ packages/zahlenspiel-ui/src/Game.tsx
@@ -1,25 +1,10 @@
 import {Room} from "colyseus.js";
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useReducer, useRef} from 'react';
 import {
     Card,
-    CardStack,
     DropCardMessage,
     FinishTurnMessage,
     GameStates,
-    isGameLostMessage,
-    isGameStartedMessage,
-    isGameWonMessage,
-    isJoinErrorMessage,
-    isJoinSuccessMessage,
-    isNewCardMessage,
-    isPlayerJoinMessage,
-    isPlayerLeaveMessage,
-    isPlayerSwitchMessage,
-    isSetupFinishedMessage,
-    isTurnFinishableMessage,
-    isTurnInvalidMessage,
-    isTurnValidMessage,
-    isUpdateCardStacksMessage,
     Player,
     StartGameMessage,
     VoteFirstTurnMessage
@@ -34,6 +19,10 @@ import ufo from "./ufo.svg";
 import comet from "./comet.svg";
 import {AppContainer, Bottom, CardDeck, CardStacks, Center, HeaderText, Top} from "./components/Layout";
 import {CardDeckComponent} from "./components/CardDeck";
+import {initialState} from "./state/ui-state";
+import {zahlenspielReducer} from "./reducers/zahlenspiel-reducer";
+import {FinishTurnAction} from "./actions/finish-turn";
+import {SelectCardStackAction} from "./actions/select-cardstack";
 
 export interface GameProps {
     room: Promise<Room>;
@@ -43,52 +32,13 @@ const Game = (props: GameProps) => {
 
     const room = useRef<Promise<Room>>(props.room);
 
-    const [self, setSelf] = useState<Player>();
-    const [currentState, setCurrentState] = useState<GameStates>(GameStates.WAITING);
-    const [couldFinish, setCouldFinish] = useState<boolean>(false);
-    const [players, setPlayers] = useState<Player[]>([]);
-    const [currentPlayer, setCurrentPlayer] = useState<Player>();
-    const [cardStacks, setCardStacks] = useState<CardStack[]>([]);
-    const [selectedCardStack, selectCardStack] = useState<CardStack>();
-    const [cardDeck, setCardDeck] = useState<Card[]>();
-    const [totalRemainingCards, setTotalRemainingCards] = useState<number>();
-    const [remainingCardsInDeck, setRemainingCardsInDeck] = useState<number>();
+    const [gameState, dispatch] = useReducer(zahlenspielReducer, initialState);
 
     useEffect(() => {
         room.current.then(r => {
             r.onMessage((message: any) => {
-                if (isJoinSuccessMessage(message)) {
-                    console.log(`Connected successfully`);
-                    setSelf(message.self);
-                } else if (isJoinErrorMessage(message)) {
-                    console.log(`Connection failed`);
-                } else if (isPlayerJoinMessage(message)) {
-                    setPlayers(message.currentPlayers);
-                } else if (isPlayerLeaveMessage(message)) {
-                    setPlayers(message.currentPlayers);
-                } else if (isPlayerSwitchMessage(message)) {
-                    setCurrentPlayer(message.currentPlayer);
-                } else if (isNewCardMessage(message)) {
-                    setCardDeck(message.cards);
-                } else if (isTurnValidMessage(message)) {
-                    setCardDeck(message.cards);
-                } else if (isTurnInvalidMessage(message)) {
-                } else if (isTurnFinishableMessage(message)) {
-                    setCouldFinish(true);
-                } else if (isUpdateCardStacksMessage(message)) {
-                    setCardStacks(message.cardStacks);
-                    setRemainingCardsInDeck(message.remainingCardsInDeck);
-                } else if (isSetupFinishedMessage(message)) {
-                    setCurrentState(GameStates.PREGAME);
-                } else if (isGameStartedMessage(message)) {
-                    setCurrentState(GameStates.GAME);
-                } else if (isGameWonMessage(message)) {
-                    setCurrentState(GameStates.WON);
-                } else if (isGameLostMessage(message)) {
-                    setTotalRemainingCards(message.remainingCards);
-                    setCurrentState(GameStates.LOST);
-                }
-            })
+                dispatch(message);
+            });
         });
     }, []);
 
@@ -97,15 +47,14 @@ const Game = (props: GameProps) => {
     };
 
     const requestFirstTurn = () => {
-        if (self) {
-            room.current.then(r => r.send(new VoteFirstTurnMessage(self?.order!)));
+        if (gameState.self) {
+            room.current.then(r => r.send(new VoteFirstTurnMessage(gameState.self?.order!)));
         }
     };
 
     const finishTurn = () => {
-        room.current.then(r => r.send(new FinishTurnMessage(self?.order!)));
-        setCouldFinish(false);
-        selectCardStack(undefined);
+        room.current.then(r => r.send(new FinishTurnMessage(gameState.self?.order!)));
+        dispatch(new FinishTurnAction())
     };
 
     const dropCard = (card: Card, stackId: string) => {
@@ -113,43 +62,43 @@ const Game = (props: GameProps) => {
     };
 
     const renderCardStacks = () => {
-        return cardStacks?.map(cardStack => {
+        return gameState.cardStacks?.map(cardStack => {
                 return (<>
                     <CardStackComponent key={`${cardStack.direction}_${cardStack.id}`}
                                         src={(cardStack.direction === "ascending") ? ufo : comet}/>
                     <CardComponent key={cardStack.id}
                                    card={new Card(cardStack.values[cardStack.values.length - 1])}
-                                   onClick={() => selectCardStack(cardStack)}
-                                   active={cardStack.id === selectedCardStack?.id}/>
+                                   onClick={() => dispatch(new SelectCardStackAction(cardStack))}
+                                   active={cardStack.id === gameState.selectedCardStack?.id}/>
                 </>)
             }
         );
     };
 
     const renderCards = () => {
-        return cardDeck?.sort((first: Card, second: Card) => first.value - second.value)
+        return gameState.cardDeck?.sort((first: Card, second: Card) => first.value - second.value)
             .filter(card => card.value)
             .map(card => {
                 return (<CardComponent key={card.value}
                                        card={card}
-                                       onClick={() => dropCard(card, selectedCardStack?.id!)}/>)
+                                       onClick={() => dropCard(card, gameState.selectedCardStack?.id!)}/>)
             });
     };
 
     const renderPlayers = () => {
-        return players.map((player: Player) => {
-            return (currentPlayer?.order === player.order) ?
+        return gameState.players.map((player: Player) => {
+            return (gameState.currentPlayer?.order === player.order) ?
                 <HighlightedPlayerView key={player.order} name={player.name} imageSize={64}/> :
                 <PlayerView key={player.order} name={player.name} imageSize={64}/>
         });
     };
 
     const finishButton = () => {
-        return (couldFinish) ? <NextButton onClick={finishTurn}/> : <></>;
+        return (gameState.couldFinish) ? <NextButton onClick={finishTurn}/> : <></>;
     };
 
     const gameComponent = () => {
-        switch (currentState) {
+        switch (gameState.currentState) {
             case GameStates.WAITING:
                 return (<>
                         <Top>
@@ -194,7 +143,7 @@ const Game = (props: GameProps) => {
                         <LobbyView>
                             {renderPlayers()}
                         </LobbyView>
-                        <CardDeckComponent numberOfCards={3}>{remainingCardsInDeck}</CardDeckComponent>
+                        <CardDeckComponent numberOfCards={3}>{gameState.remainingCardsInDeck}</CardDeckComponent>
                     </Center>
                     <Bottom>
                         <CardDeck>
@@ -224,7 +173,8 @@ const Game = (props: GameProps) => {
                             {renderCardStacks()}
                         </CardStacks>
                     </Top>
-                    <Center>Oh no, you got beaten by the game! There were {totalRemainingCards} cards left!</Center>
+                    <Center>Oh no, you got beaten by the game! There were {gameState.totalRemainingCards} cards
+                        left!</Center>
                     <Bottom>
                         <CardDeck>
                             {renderCards()}
