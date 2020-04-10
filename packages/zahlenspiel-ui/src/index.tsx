971c0c189351d928d143bfaeda0fe6d974ad20fd
Simon Hofmann <mail@simon-hofmann.org>
2020-04-10 19:07:22 +0200

    Restructured huge parts to use connection manager, direct joins via room hash are now also possible

--- packages/zahlenspiel-ui/src/index.tsx
+++ packages/zahlenspiel-ui/src/index.tsx
@@ -1,18 +1,12 @@
 import * as Colyseus from "colyseus.js";
-import {Room} from "colyseus.js";
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 import './index.css';
-import Game from './Game';
 import * as serviceWorker from './serviceWorker';
-import {MAX_RECONNECTION_TIME_IN_SECONDS} from "zahlenspiel-shared-entities";
 import {GameErrorBoundary} from "./GameErrorBoundary";
-
-const Storage = {
-    sessionId: "zahlenspiel_sessionId",
-    roomId: "zahlenspiel_roomId",
-    lastUpdated: "zahlenspiel_lastUpdated",
-};
+import {ConnectionHandler} from "./connection-handler";
+import Zahlenspiel from "./Zahlenspiel";
+import {getRoomId} from "./game-data-handler";
 
 let endpoint = `${window.document.location.protocol.replace("http", "ws")}//${window.document.location.hostname}`;
 if (window.document.location.port && window.document.location.port !== "80") {
@@ -21,83 +15,20 @@ if (window.document.location.port && window.document.location.port !== "80") {
     endpoint = `${endpoint}:2567`;
 }
 
-const setState = (roomId: string, sessionId: string) => {
-    localStorage.setItem(Storage.roomId, roomId);
-    localStorage.setItem(Storage.sessionId, sessionId);
-    localStorage.setItem(Storage.lastUpdated, String(Date.now()));
-};
+const connectionHandler = new ConnectionHandler(
+    new Colyseus.Client(endpoint)
+);
 
-const isStateOutdated = () => {
-    const now = Date.now();
-    const lastUpdate = localStorage.getItem(Storage.lastUpdated);
-    if (!lastUpdate) {
-        return true;
-    } else {
-        const timeSinceLastStateUpdateInSeconds = (now - Number(lastUpdate)) / 1000;
-        return (timeSinceLastStateUpdateInSeconds > MAX_RECONNECTION_TIME_IN_SECONDS);
-    }
-};
-
-const getState = () => {
-    return ({
-        roomId: localStorage.getItem(Storage.roomId),
-        sessionId: localStorage.getItem(Storage.sessionId),
-        lastUpdated: localStorage.getItem(Storage.lastUpdated)
-    });
-};
-
-const clearState = () => {
-    for (const val of Object.values(Storage)) {
-        localStorage.removeItem(val);
-    }
-};
-
-const connect = () => {
-    console.log(`Connecting to endpoint: ${endpoint}`);
-    const playerName = window.prompt("Please enter your name", "Guy Incognito");
-    const room = client.joinOrCreate('zahlenspiel', {playerName});
-    return room.then(r => {
-        setState(r.id, r.sessionId);
-        return r;
-    });
-};
-
-const reconnect = (roomId: string, sessionId: string) => {
-    console.log(`Reconnecting to endpoint: ${endpoint}`);
-    return client.reconnect(roomId, sessionId).then(r => {
-        return r;
-    })
-};
-
-const client = new Colyseus.Client(endpoint);
-let room: Promise<Room>;
-
-if (!isStateOutdated()) {
-    const answer = window.confirm("You have an ongoing game, do you want to reconnect?");
-    const storedGameState = getState();
-    if (answer) {
-        if (storedGameState.roomId && storedGameState.sessionId) {
-            room = reconnect(storedGameState.roomId, storedGameState.sessionId);
-        } else {
-            clearState();
-            room = Promise.reject(`Missing room and / or session info, failed to reconnect.`);
-        }
-    } else {
-        if (storedGameState.roomId && storedGameState.sessionId) {
-            room = reconnect(storedGameState.roomId, storedGameState.sessionId);
-            room.then(r => r.leave(true));
-        }
-        room = connect()
-    }
-} else {
-    clearState();
-    room = connect()
+const directJoinRoomId = getRoomId();
+if (directJoinRoomId) {
+    console.log("direct join room:", directJoinRoomId);
+    connectionHandler.join(directJoinRoomId);
 }
 
-ReactDOM.render(
+render(
     <React.StrictMode>
         <GameErrorBoundary>
-            <Game room={room}/>
+            <Zahlenspiel connectionHandler={connectionHandler}/>
         </GameErrorBoundary>
     </React.StrictMode>,
     document.getElementById('root')
