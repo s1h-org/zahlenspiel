307b766ddb1333b44a571bbd0b025be261c2c594
Simon Hofmann <mail@simon-hofmann.org>
2020-04-12 12:26:19 +0200

    Continued writing tests for zahlenspiel-reducer

--- packages/zahlenspiel-ui/src/reducers/zahlenspiel-reducer.spec.ts
+++ packages/zahlenspiel-ui/src/reducers/zahlenspiel-reducer.spec.ts
@@ -1,18 +1,22 @@
 import {initialState} from "../state/ui-state";
 import {
+    Card,
     JoinErrorMessage,
-    JoinSuccessMessage,
+    JoinSuccessMessage, NewCardMessage,
     Player,
     PlayerJoinMessage,
-    PlayerLeaveMessage
+    PlayerLeaveMessage, PlayerSwitchMessage, TurnFinishableMessage
 } from "zahlenspiel-shared-entities";
 import {zahlenspielReducer} from "./zahlenspiel-reducer";
+import {Simulate} from "react-dom/test-utils";
+import play = Simulate.play;
 
 describe("zahlenspiel-reducer", () => {
     it("should update self on joinSuccess", () => {
         // GIVEN
         const player = new Player(0, "testPlayer");
         const incomingMessage = new JoinSuccessMessage(player)
+        initialState.self = undefined;
 
         // WHEN
         const updatedState = zahlenspielReducer(initialState, incomingMessage);
@@ -35,25 +39,85 @@ describe("zahlenspiel-reducer", () => {
         // GIVEN
         const playerOne = new Player(0, "first");
         const playerTwo = new Player(1, "second");
+        const newPlayers = [playerOne, playerTwo];
+        initialState.players = [];
 
         // WHEN
-        const updateState = zahlenspielReducer(initialState, new PlayerLeaveMessage([playerOne, playerTwo]));
+        const updateState = zahlenspielReducer(initialState, new PlayerLeaveMessage(newPlayers));
 
         // THEN
-        expect(updateState.players.length).toBe(2);
-        expect(updateState.players).toEqual(expect.arrayContaining([playerOne, playerTwo]));
+        expect(updateState.players.length).toBe(newPlayers.length);
+        expect(updateState.players).toEqual(expect.arrayContaining(newPlayers));
     });
 
     it("should update player list on player join", () => {
         // GIVEN
         const playerOne = new Player(0, "first");
         const playerTwo = new Player(1, "second");
+        const newPlayers = [playerOne, playerTwo];
+        initialState.players = [];
 
         // WHEN
-        const updateState = zahlenspielReducer(initialState, new PlayerJoinMessage([playerOne, playerTwo]));
+        const updateState = zahlenspielReducer(initialState, new PlayerJoinMessage(newPlayers));
 
         // THEN
-        expect(updateState.players.length).toBe(2);
-        expect(updateState.players).toEqual(expect.arrayContaining([playerOne, playerTwo]));
+        expect(updateState.players.length).toBe(newPlayers.length);
+        expect(updateState.players).toEqual(expect.arrayContaining(newPlayers));
+    });
+
+    it("should update current player on player switch", () => {
+        // GIVEN
+        const currentPlayer = new Player(0, "current");
+        const nextPlayer = new Player(1, "next");
+        initialState.currentPlayer = currentPlayer;
+
+        // WHEN
+        const updateState = zahlenspielReducer(initialState, new PlayerSwitchMessage(nextPlayer));
+
+        // THEN
+        expect(updateState.currentPlayer).toEqual(nextPlayer);
+    });
+
+    it("should update list of cards on newCard message", () => {
+        // GIVEN
+        const currentCards: Card[] = [];
+        const cardTen = new Card(10);
+        const cardTwelve = new Card(12);
+        const newCards = [cardTen, cardTwelve];
+        initialState.cardDeck = currentCards;
+
+        // WHEN
+        const updateState = zahlenspielReducer(initialState, new NewCardMessage(newCards));
+
+        // THEN
+        expect(updateState.cardDeck.length).toBe(2);
+        expect(updateState.cardDeck).toEqual(expect.arrayContaining(newCards));
+    });
+
+    it("should update list of cards on valid turn", () => {
+        // GIVEN
+        const cardTen = new Card(10);
+        const cardTwelve = new Card(12);
+        const currentCards = [cardTen, cardTwelve];
+        const newCards = [cardTen];
+        initialState.cardDeck = currentCards;
+
+        // WHEN
+        const updateState = zahlenspielReducer(initialState, new NewCardMessage(newCards));
+
+        // THEN
+        expect(updateState.cardDeck.length).toBe(1);
+        expect(updateState.cardDeck).toEqual(expect.arrayContaining(newCards));
+    });
+
+    it("should mark turn as finishable on isFinishable message", () => {
+        // GIVEN
+        initialState.couldFinish = false;
+
+        // WHEN
+        const updateState = zahlenspielReducer(initialState, new TurnFinishableMessage());
+
+        // THEN
+        expect(updateState.couldFinish).toBeTruthy();
     });
 });
