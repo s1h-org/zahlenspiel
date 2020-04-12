5b8fcb40e806ef238f291961c6bf45707fc2636e
Simon Hofmann <mail@simon-hofmann.org>
2020-04-12 13:00:11 +0200

    Updated actions typeguards

--- packages/zahlenspiel-ui/src/actions/finish-turn.ts
+++ packages/zahlenspiel-ui/src/actions/finish-turn.ts
@@ -1,6 +1,6 @@
 import {ActionType, BaseAction, createTypeGuard} from "./game-actions";
 
-export const isFinishTurnAction = createTypeGuard("finish-turn");
+export const isFinishTurnAction = createTypeGuard<FinishTurnAction>("finish-turn");
 
 export class FinishTurnAction extends BaseAction {
     type: ActionType = "finish-turn";
